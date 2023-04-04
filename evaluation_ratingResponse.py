828a956ac6d7209061155db8c8611832c0c4fde4
Rauf Iftikhar <riftikh@emory.edu>
2023-04-04 19:16:25 -0400

    Format rating response

--- evaluation_ratingResponse.py
+++ evaluation_ratingResponse.py
@@ -1,5 +1,6 @@
 import openai
-PATH_API_KEY = 'resources/noah_api_key.txt'
+import json
+PATH_API_KEY = 'resources/open_ai.txt'
 openai.api_key_path = PATH_API_KEY
 
 def rateResponseOnDescriptionTurbo(job_description, statement):
@@ -14,20 +15,32 @@ def rateResponseOnDescriptionTurbo(job_description, statement):
     return printedText
 
 def main():
-    N_job_description = """
-           Responsibilities:
-       * Provide technical support to customers
-       * Install and train customers on systems
-       * Test and quality assure products
-       Requirements:
-       * Bachelor of Science degree
-       * Strong communication and problem solving skills
-       * Willingness to travel domestically and internationally
-       Desired skills:
-       * Background in motion capture technology
-       * Experience with Matlab, Python or Visual 3D
-       * Familiarity with software such as Nexus, Unity, Unreal, etc.
-       """
+
+    with open('job_descriptions.json', 'r') as f:
+        data = json.load(f)
+
+    jobs = list(data.keys())
+    job = jobs[2] #arbitrarily pick the third element from the list
+
+    N_job_description = str(data[job])
+
+    # Write the updated dictionary back to the JSON file!
+    # with open('resources/previous.json', 'w') as f:
+    #     json.dump(data, f)
+    # N_job_description = """
+    #        Responsibilities:
+    #    * Provide technical support to customers
+    #    * Install and train customers on systems
+    #    * Test and quality assure products
+    #    Requirements:
+    #    * Bachelor of Science degree
+    #    * Strong communication and problem solving skills
+    #    * Willingness to travel domestically and internationally
+    #    Desired skills:
+    #    * Background in motion capture technology
+    #    * Experience with Matlab, Python or Visual 3D
+    #    * Familiarity with software such as Nexus, Unity, Unreal, etc.
+    #    """
 
     print('RESPONSE SCORING \n')
     print('===CASE 1=== \n')
