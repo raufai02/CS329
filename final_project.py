43a3cc1e7125d6191ac1b7630d238e9b33488e3e
Rauf Iftikhar <riftikh@emory.edu>
2023-04-05 19:59:25 -0400

    clean up

--- final_project.py
+++ final_project.py
@@ -56,23 +56,6 @@ class MacroGetLittleQuestion(Macro:
 
 
 
-class MacroCallGPT(Macro):
-    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
-        PATH_API_KEY = 'resources/openai_api.txt'
-        openai.api_key_path = PATH_API_KEY
-
-        model = 'gpt-3.5-turbo'
-        content = 'Say something inspiring'
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=[{'role': 'user', 'content': content}]
-        )
-
-        return response["choices"]["message"]["content"]
-
-
-
-
 class MacroGreet(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[str]):
 
