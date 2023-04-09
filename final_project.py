bed74ab04ce9442d7b181803e76c563a3b9b120b
Rauf Iftikhar <riftikh@emory.edu>
2023-04-09 19:00:13 -0400

    Sunday edits

--- final_project.py
+++ final_project.py
@@ -77,10 +77,10 @@ class MacroLoadScores(Macro):
         with open(filename, 'r') as f:
             data = json.load(f) #load the scoring file into the workspace!
 
-        vars["total_score"] = data["Total Score"]
-        vars["emotion_score"] = data["Emotion Score"]
-        vars["context_score"] = data["Context Score"]
-        vars["requirement_score"] = data["Requirement Score"]
+        vars["TOTAL_SCORE"] = data["Total Score"]
+        vars["EMOTION_SCORE"] = data["Emotion Score"]
+        vars["CONTEXT_SCORE"] = data["Context Score"]
+        vars["REQUIREMENT_SCORE"] = data["Requirement Score"]
 
 class MacroGetExample(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
@@ -92,7 +92,6 @@ class MacroGetExample(Macro):
         positive = data["Positive Examples"][1] # should return an index
 
         negative = data["Negative Examples"][2].Reason #returns a sentence reasoning
-
         return negative
 
 class MacroStoreResponse(Macro): #store the last response!
@@ -130,64 +129,26 @@ class MacroGetBigQuestion(Macro):
         return question
 class MacroGetLittleQuestion(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
-        global dialogue
+        global dialogue, dialogue_counter
 
         if len(vars["follow_ups"]) == 0:
             vars["Q_REMAIN"] = False
             vars["NO_FOLLOWUP"] = True
 
-
-        #str = 'That should be good enough to cover $CURR STATE
-            str= 'OK. All of that is good to hear'
+        #response = 'That should be good enough to cover $CURR STATE
+            response = 'OK. All of that is good to hear'
 
             dialogue_counter = dialogue_counter + 1
-            dialogue.append(str(dialogue_counter) + ' S: ' + str)
-            return str
+            dialogue.append(str(dialogue_counter) + ' S: ' + response)
+            return response
         else:
             res = random.choice(vars["follow_ups"])
             idx= vars["follow_ups"].index(res)
             vars["follow_ups"].pop(idx)
             vars["Q_REMAIN"] = True
             vars["NO_FOLLOWUP"] = False
-            dialogue.append('S: ' + res)
+            dialogue.append(str(dialogue_counter) + ' S: ' + res)
             return res
-
-class MacroGreet(Macro):
-    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[str]):
-
-        url = 'https://api.weather.gov/gridpoints/FFC/52,88/forecast'
-        r = requests.get(url)
-        d = json.loads(r.text)
-        periods = d['properties']['periods']
-        today = periods[0]
-        #return today['detailedForecast']
-        forecast = today['detailedForecast']
-        if 'sunny' in forecast.lower():
-            weather= 'sunny'
-        elif 'cloudy' in forecast.lower():
-            weather = 'cloudy'
-        elif 'rain' in forecast.lower() or 'shower' in forecast.lower():
-            weather = 'rainy'
-        else:
-            weather = 'nice'
-
-        # ^Note you could just use the shortform forecast instead of casting everything with if statements
-        # See shortForecast in API call. Can be called; today[shortForecast] will return one line forecast
-        #--Noah
-
-        current_time = time.localtime()
-        time_str = "day" #default ..?
-        if current_time.tm_hour < 12:
-            time_str = "morning"
-        elif current_time.tm_hour < 17:
-            time_str =  "afternoon"
-        else:
-            time_str = "evening"
-
-        greetings = ["What should I call you?", "What is your name?", "What do you go by?", "How should I refer to you?"]
-        random_str = random.choice(greetings)
-        return "Good " + time_str + "; It's " + weather + " today! " + random_str #What should I call you?"
-
 
 def interviewBuddy() -> DialogueFlow:
     transitions = { #classification state
@@ -232,8 +193,8 @@ def interviewBuddy() -> DialogueFlow:
         'WHAT_ELSE': MacroWhatElse(),
         'SETBOOL': MacroSetBool(),
         'ENCOURAGEMENT': MacroEncourage(), 
-        'PERSONA' : MacroPersona()
-        'RUN_EVAL' : MacroLoadScores(),
+        'PERSONA' : MacroPersona(),
+        'RUN_EVAL' : MacroLoadScores()
 
     }
 
@@ -269,21 +230,6 @@ def save(df: DialogueFlow, d: List[Any]): #d is the dialogue list
     fout = open(filename, 'w')
     fout.write('\n'.join(d))
 
-
-
-
-
-# def save(df: DialogueFlow, varfile: str):
-#     df.run()
-#     d = {k: v for k, v in df.vars().items() if not k.startswith('_')}
-#     pickle.dump(d, open(varfile, 'wb'))
-
-
-# def load(dialogue: List[Any], varfile: str):
-#     d = pickle.load(open(varfile, 'rb'))
-#     df.vars().update(d)
-#     df.run()
-#     save(df, varfile)
 
 if __name__ == '__main__':
     interviewBuddy().run()
